812559c38225d0bf5de99001dd2207b1f385523f
siljaer <enricovkon@gmail.com>
2017-06-03 11:55:17 +0200

    trunk: fix 64bit build
    Change-Id: Icf5e0e94fc34bc72108eacbfba13ba95999fae92

--- omni_trunk.mk
+++ omni_trunk.mk
@@ -24,6 +24,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
