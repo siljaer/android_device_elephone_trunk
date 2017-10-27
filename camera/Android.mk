790da3ec06896dad2af004b3d9d465f7dc54287d
siljaer <enricovkon@gmail.com>
2017-10-27 13:31:36 +0200

    camera: use CM camera hal (from ottmi's tree)

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,4 @@
-#ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 MM_V4L2_DRIVER_LIST += msm8960
 MM_V4L2_DRIVER_LIST += msm8974
 MM_V4L2_DRIVER_LIST += msm8226
@@ -7,9 +7,9 @@ MM_V4L2_DRIVER_LIST += msm_bronze
 MM_V4L2_DRIVER_LIST += msm8916
 MM_V4L2_DRIVER_LIST += msm8909
 LOCAL_32_BIT_ONLY := true
-#ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
+ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
     endif
-#endif
-#endif
+endif
+endif
